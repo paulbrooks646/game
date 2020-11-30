c749288db7278b01569afd875624ccbd6552fd46
paulbrooks646 <paulbrooks646@gmail.com>
2020-11-30 16:59:49 -0700

    thief picture

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,5 +1,5 @@
 import react from "react";
-import "./Dashboard.css"
+import "./Dashboard.css";
 
 export default function dashboard() {
   return (
@@ -7,14 +7,14 @@ export default function dashboard() {
       <h1 className="dashboard-title">Welcome to Shining Quest</h1>
       <h2 className="dashboard-subtitle">Choose your character:</h2>
       <div className="dashboard-characters">
-        <div className="dashboard-character">
-          <h3>Thief</h3>
+        <div className="dashboard-character" style={{backgroundImage: 'url("https://upload.wikimedia.org/wikipedia/en/b/be/Aladdin_Disney_pose.png")'}}>
+          <h3 className="character-type">Thief</h3>
         </div>
         <div className="dashboard-character">
-          <h3>Warrior</h3>
+          <h3 className="character-type">Warrior</h3>
         </div>
         <div className="dashboard-character">
-          <h3>Wizard</h3>
+          <h3 className="character-type">Wizard</h3>
         </div>
       </div>
     </div>
